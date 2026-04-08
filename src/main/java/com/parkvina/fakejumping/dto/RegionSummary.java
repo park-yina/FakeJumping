fef47d19523de8c06b477b47f8d4fd0ffd2f3879
park-yina <kln99988@gamil.com>
2026-04-08 21:11:52 +0900

    docs: sync README from dev

---
+++ src/main/java/com/parkvina/fakejumping/dto/RegionSummary.java
@@ -0,0 +1,11 @@
+package com.parkvina.fakejumping.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RegionSummary {
+    String region;
+    int count;
+}
